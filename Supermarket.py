4062ca267191cbc4c4ee45d88427fb6293b7ca36
ditarnnt <68722344+ditarnnt@users.noreply.github.com>
2023-01-23 20:47:52 +0700

    adding feature for view item

--- Supermarket.py
+++ Supermarket.py
@@ -1,15 +1,19 @@
 print ("Welcome to supermarket")
 print(input("Press Enter to Continue "))
-print ("1.Add item 2.Update Item 3.Delete Item 4.Reset Item")
+def menu ():
+    print ("1.Add item\n 2.Update Item\n 3.Delete Item\n 4.Reset Item\n")
+    return
+
 option = int(input("Enter the option: "))
 
 items = []
 item ={}
 def add_item (): 
+    print ("----- add groceries -----")
     while True:
         item['name'] = str(input('Enter item name to buy (q to menu) : '))
         if item['name'].lower() == 'q':
-            break
+            break #back to menu 
         else: 
             while True:
                 try:
@@ -25,9 +29,18 @@ def add_item ():
                     print('prices per item should in digits')
         return (items.append(item))
 
-def 
+def view_item ():
+    print("----- update groceries -----")
+    print(f"These are the list of your Grocery, you have: {len({items})}")
+    while len(items)!= 0:
+        print ("Your Groceries's list")
+        for item in items:
+            for key,value in item.items():
+                print(key,':',value)
+            break
+    
 
-add_item ()
+
 
 
 
